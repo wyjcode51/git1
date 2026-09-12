e0568ef5236b91b88ea3eb63dc695f105c8738e7
wyjcode51 <315808837+wyjcode51@users.noreply.github.com>
2026-09-12 14:15:56 +0800

    feat: 添加ValueDemo1和ValueDemo2变量演示代码

---
+++ ValueDemo1.java
@@ -0,0 +1,20 @@
+public class ValueDemo1{
+	public static void main(String[] args){
+		//学习字面量
+		//整数
+		System.out.println("888");
+		//小数
+		System.out.println("8.88");
+		//字符串
+		System.out.println("葡萄成熟时");
+		//字符
+		System.out.println('神');
+		//布尔
+		System.out.println(true);
+		System.out.println(false);
+		//null
+		//null不能打印
+		System.out.println("null");
+		
+	}
+}
---
+++ ValueDemo2.java
@@ -0,0 +1,16 @@
+public class ValueDemo2{
+	//学习变量
+	public static void main(String[]args){
+		//1.定义
+		int a = 1;
+		double b = 1.1;
+		System.out.println(a);
+		System.out.println(b);
+		//2.计算
+		int c = 1;
+		int n = 2;
+		System.out.println(c+n);
+	}
+	
+	
+}
